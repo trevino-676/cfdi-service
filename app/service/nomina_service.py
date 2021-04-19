b93d83bfcadddbcf1f52572fe00890abee0fe8f3
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-04-19 12:56:12 -0500

    Se agrega un metodo en el servicio para buscar la informacion de giro que coincida con la nomina

--- app/service/nomina_service.py
+++ app/service/nomina_service.py
@@ -2,6 +2,7 @@
 author: Luis Manuel Torres Trevino
 date: 12/04/2021
 """
+from app.models.giro_model import Giro
 from app.service import Service
 from app.repository import Repository
 
@@ -11,7 +12,30 @@ class NominaService(Service):
         self.repository = repo
 
     def find_one(self, filters: dict) -> dict:
-        return self.repository.find_one(filters)
+        nomina = self.repository.find_one(filters)
+        giro = self.__find_giro_data(nomina._id)
+        return nomina.response_data(giro)
 
-    def find(self, filters: dict) -> dict:
-        return self.repository.find(filters)
+    def find(self, filters: dict) -> list:
+        nominas = self.repository.find(filters)
+        result_nomina = []
+        for nomina in nominas:
+            giro = self.__find_giro_data(nomina._id)
+            result_nomina.append(nomina.response_data(giro))
+        return result_nomina
+
+    def __find_giro_data(self, uuid: str) -> dict:
+        """
+        Busca la informacion de giro en la base de datos relacionada
+        con el uuid del documento de nominas.
+        :param uuid: uuid del documento a buscar
+        :return: diccionario con la informacion de giro.
+        """
+        filter = {"uuid": uuid}
+        try:
+            giro = Giro()
+            giro.find(filter)
+            return giro
+        except Exception as e:
+            print(e)
+            return None
