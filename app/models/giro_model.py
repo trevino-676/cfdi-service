69d784ae76c7f6a10324e73b3a58bb8290d5a456
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-05-10 23:32:46 -0500

    Se agrego el metodo para guardar especifico para giro.

--- app/models/giro_model.py
+++ app/models/giro_model.py
@@ -8,3 +8,9 @@ from app.models import Model
 
 class Giro(Model):
     collection = mongo.db[app.config["GIRO_COLLECTION"]]
+
+    def save(self):
+        if not self.uuid:
+            self.collection.insert_one(self)
+        else:
+            self.collection.replace_one({"uuid": self.uuid}, self)
