1cea85ddf48c04a131f986eb8f7bbc89c4ad6644
david.iniguez <david.iniguez@drumbot.com.mx>
2021-08-14 16:13:55 -0500

    Se agrego APIs para graficas del dashboard

---
+++ app/routes/principal_routes.py
@@ -0,0 +1,173 @@
+"""
+author: ErrataSEV
+date: 12/08/2021
+"""
+from flask import Blueprint, make_response, request
+from bson.json_util import dumps
+
+from app.service import principal_service
+from app.utils import FilterType, make_filters, validate_params
+from app import app
+
+principal_routes = Blueprint("principal", __name__, url_prefix="/v1/principal")
+
+
+@principal_routes.route("/", methods=["GET"])
+def find_principal():
+    """
+    Busca un solo documento de ingresos que coincida con los filtros
+    """
+    parameters = request.args
+    try:
+        if validate_params(parameters):
+            cfdi = principal_service.find_one(
+                make_filters(
+                    FilterType.AND if parameters["type"] == "and" else FilterType.OR,
+                    parameters["filters"],
+                )
+            )
+        else:
+            cfdi = principal_service.find_one({})
+    except Exception as e:
+        print(e)
+        message = str(e)
+        cfdi = None
+
+    if cfdi is None:
+        resp = make_response(
+            dumps(
+                {
+                    "status": False,
+                    "message": message
+                    if message
+                    else "No se encontro ningun recibo de cfdi",
+                }
+            ),
+            404,
+        )
+    else:
+        resp = make_response(dumps({"status": True, "data": cfdi}), 200)
+    resp.headers["Content-Type"] = "application/json"
+    return resp
+
+
+@principal_routes.route("/all", methods=["POST"])
+def find_all_cfdis():
+    """find_all_cfdis
+    Busca todos los documentos que coincidan con los filtros
+    """
+    parameters = request.form.to_dict()
+    filters = {}
+    for k,v in parameters.items():
+        if v == "null" :
+            filters[k] = None
+        else:
+            filters[k] = v
+    
+    cfdis = principal_service.find(filters)
+
+    if cfdis is None or len(cfdis) == 0:
+        resp = make_response(
+            dumps(
+                {"status": False, "message": "No se encontro ningun recibo de nomina"}
+            ),
+            404,
+        )
+    else:
+        resp = make_response(dumps({"status": True, "data": cfdis}), 200)
+
+    resp.headers["Content-Type"] = "application/json"
+    return resp
+
+@principal_routes.route("/get-group", methods=["POST"])
+def find_data_basics():
+    """find_all_cfdis
+    Busca todos los documentos que coincidan con los filtros
+    """
+
+    parameters = request.form.to_dict()
+    filters = {}
+    for k,v in parameters.items():
+        if v == "null" :
+            filters[k] = None
+        else:
+            filters[k] = v
+    response = {"top":[],"data":[]}
+    try:
+        cfdis = principal_service.aggregate([
+            {"$match": {
+                    filters["fieldMatch"]: filters["user"],
+                    "datos.Fecha": {
+                        "$gte": filters["dateBegin"],
+                        "$lte": filters["dateEnd"]
+                    }
+            }},
+            {"$group": {
+                "_id": "$"+filters["fieldGroup"], 
+                "count": {"$sum":1}
+            }}
+        ])
+        response["data"] = [filters["data"].split(",")]
+    except Exception as e:
+        print(e)
+        message = str(e)
+        cfdi = None
+    if cfdis is None or len(cfdis) == 0:
+        resp = make_response(
+            dumps(
+                {"status": False, "message": "No se encontro ningun recibo de nomina"}
+            ),
+            404,
+        )
+    else:
+        print(cfdis)
+        for pair in cfdis:
+            response["top"].append(pair["_id"])
+            response["data"].append([pair["_id"], pair["count"]])
+        resp = make_response(dumps({"status": True, "data": response}), 200)
+
+    resp.headers["Content-Type"] = "application/json"
+    return resp
+
+@principal_routes.route("/get-count", methods=["POST"])
+def data_count():
+    """data_count
+    Hace un conteo de los documentos que coincidan con los filtros
+    """
+    
+    parameters = request.form.to_dict()
+    try:
+        cfdis = principal_service.aggregate([
+            {"$match": {
+                    parameters["fieldMatch"]: parameters["user"],
+                    "datos.Fecha": {
+                        "$gte": parameters["dateBegin"],
+                        "$lte": parameters["dateEnd"]
+                    },
+                    "datos.Cancelado": None
+            }},
+            {"$group": {
+                "_id": "$"+parameters["fieldMatch"], 
+                "count": {"$sum":1},
+                "total": {"$sum": "$datos.Total"},
+                "subtotal": {"$sum": "$datos.SubTotal"}
+            }}
+        ])
+    except Exception as e:
+        print(e)
+        message = str(e)
+        cfdi = None
+    if cfdis is None or len(cfdis) == 0:
+        resp = make_response(
+            dumps(
+                {"status": False, "message": "No se encontro ningun recibo de nomina"}
+            ),
+            404,
+        )
+    else:
+        resp = make_response(dumps({"status": True, "data": cfdis}), 200)
+
+    resp.headers["Content-Type"] = "application/json"
+    return resp
+
+
--- app/service/__init__.py
+++ app/service/__init__.py
@@ -1,7 +1,9 @@
 from app.service.service import Service
-from app.repository import repository, giro_repo
+from app.repository import repository, giro_repo, principal_repo
 from app.service.nomina_service import NominaService
 from app.service.giro_service import GiroService
+from app.service.principal_service import PrincipalService
 
 service = NominaService(repository)
 giro_service = GiroService(giro_repo)
+principal_service = PrincipalService(principal_repo)
